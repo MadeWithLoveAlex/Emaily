22dbb388f8e8e499cfa495b7754eea288d1f3512
MadeWithLoveAlex <rony.aguilar.dev@gmail.com>
2022-07-31 19:52:34 -0700

    refactored files

---
+++ services/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
+const keys = require('../config/keys')
+
+passport.use(
+    new GoogleStrategy(
+        {
+            clientID: keys.GOOGLE_CLIENT_ID,
+            clientSecret: keys.GOOGLE_CLIENT_SECRET,
+            callbackURL: '/auth/google/callback'
+        },
+        (accessToken) => {
+            console.log(accessToken)
+        }
+    )
+)
