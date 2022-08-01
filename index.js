b6ad41529cf95a2a71fb0e6f9ed6169b8702d27a
MadeWithLoveAlex <rony.aguilar.dev@gmail.com>
2022-08-01 13:31:24 -0700

    add prod keys and finished auth flow

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require('express')
+const cookieSession = require('cookie-session')
 const mongoose = require('mongoose')
+const passport = require('passport')
 
 const keys = require('./config/keys')
 
 require('./models/User')
 require('./services/passport')
 
+mongoose.connect(keys.MONGO_URI)
 const app = express()
-mongoose.connect(keys.mongoURI)
+
+app.use(cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.COOKIE_KEY]
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
 
 require('./routes/authRoutes')(app)
 
