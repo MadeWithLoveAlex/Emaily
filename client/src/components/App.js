e6672b476c37587c147fe03808988a7766d42b29
Alex Aguilar <rony.aguilar.dev@gmail.com>
2022-08-14 15:53:29 -0700

    added header content for auth handling and landing page

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,8 +4,10 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import { fetchUserAsync } from '../reducers/authSlice'
 
 import Header from './Header'
+import {Landing} from './Landing'
+import 'materialize-css/dist/css/materialize.min.css'
+
 const Dashboard = () => (<h2>Dashboard</h2>)
-const Landing = () => (<h2>Landing</h2>)
 const SurveyNew = () => (<h2>SurveyNew</h2>)
 
 class App extends Component {
@@ -18,7 +20,7 @@ class App extends Component {
         return (
             <div>
                 <BrowserRouter>
-                    <div>
+                    <div className='container'>
                         <Header />
                         <Route exact path='/' component={Landing}></Route>
                         <Route exact path='/surveys' component={Dashboard}></Route>
